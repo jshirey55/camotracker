a96818c565edc6bded2a28b2689690057e78ec5a
Joshua Shirey <jshirey5555@gmail.com>
2026-01-06 14:28:52 -0500

    added poe image to homepage that links to poe page, realized missing content from main pc and dont want to cause any conflicts

--- camotrackerapp/src/components/HomePage.jsx
+++ camotrackerapp/src/components/HomePage.jsx
@@ -1,7 +1,15 @@
 import React from "react"
+import { Routes, Route } from "react-router-dom"
+import "./styles/styles.css"
 
 const Home = () => {
-    return <h1>Home Page</h1>
+    
+    return (
+    <div>
+        <h1>Home Page</h1>
+    <a href="/pathofexile"><img  className="homepagepoeimg" src="https://web.poecdn.com/protected/image/poe2/announcements/the-last-druids/the-last-of-the-druids-logo-en.webp?key=nziFodPpsDt4OkksmPBwpw" alt="Path of Exile 2" /></a>
+    </div>
+    )
 }
 
 export default Home

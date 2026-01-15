d9f387429ffaa12798698ed25550e4fa5a10f071
jshirey55 <jshirey5555@gmail.com>
2026-01-15 08:23:39 -0500

    added live character component and homepage styling

--- camotrackerapp/src/components/PathOfExile.jsx
+++ camotrackerapp/src/components/PathOfExile.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Rings from "./pathofexilecomponents/Rings";
 import Amulets from "./pathofexilecomponents/Amulets";
+import LiveCharacter from "./pathofexilecomponents/LiveCharacter";
 import "./styles/Pathofexile.css"
 
 export default function PathOfExile() {
     return (
         <div className="path-of-exile-page">
             <h1>Path Of Exile</h1>
+            <LiveCharacter />
             <Rings />
             <Amulets />
         </div>
---
+++ camotrackerapp/src/components/pathofexilecomponents/LiveCharacter.jsx
@@ -0,0 +1,13 @@
+import "../styles/Pathofexile.css"
+
+export default function LiveCharacter() {
+    return (
+        <div className="live-character-container">
+            <h1 className="live-character-header">Live Character</h1>
+            <a className="character" href="https://poe.ninja/poe2/profile/Elucidous-3356/character/ElucimetaLOL">
+            <img className="character" src="https://poe.ninja/poe2-assets/cdn/classes/pathfinder.webp" alt="Live Character" />
+            <div>Live Character</div>
+            </a>
+        </div>
+    )
+}
